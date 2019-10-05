0c4d3b610a62a58856383bc23caa0088480283fa
claus diwisch <claus.diwisch@gmail.com>
2019-10-05 10:58:06 +0200

    Update crape.js

--- crape.js
+++ crape.js
@@ -1,1 +1,12 @@
-ja
+Module.register("MMM-crape",{
+	defaults: {
+		url: "https://fm4.orf.at/player/live"
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = this.config.url;
+		return wrapper;
+	}
+});
