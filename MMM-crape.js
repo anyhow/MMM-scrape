b1f528835901f0061a5dafff149ac0fb7917b41b
claus diwisch <claus.diwisch@gmail.com>
2019-10-05 12:58:21 +0200

    Update MMM-crape.js

--- MMM-crape.js
+++ MMM-crape.js
@@ -3,7 +3,9 @@ Module.register("MMM-crape",{
 		url: "https://fm4.orf.at/player/live",
 		domselector: "span.playing"
 	},
-
+	
+const JSDOM = require('jsdom').JSDOM;
+	
 	getDom: function() {
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = "HAALLoo";
